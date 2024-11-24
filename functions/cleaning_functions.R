d25dad0d7876e60f223be92855f4769cbb1821f9
Sulaiman Anwary <54301392+Sulaiman136@users.noreply.github.com>
2024-11-24 17:01:32 +0300

    Update cleaning_functions.R

--- functions/cleaning_functions.R
+++ functions/cleaning_functions.R
@@ -167,3 +167,81 @@ detect.outliers <- function(df, method="sd", n.sd=3){
   return(res)
 }
 
+
+#------------------------------------------------------------------------------------------------------------------------------------------
+# check interview duration
+
+check_int_duration <- function(df_raw, x_uuid="_uuid", audit_dir_path = "", today = "today"){
+  # validation and error handling
+  if (!today %in% names(df_raw))
+    stop(paste0("Column ", today, " doesn't exist in the data set."))
+  if (!"start" %in% names(df_raw))
+    stop("Column start doesn't exist in the data set.")
+  if (!"end" %in% names(df_raw))
+    stop("Column end doesn't exist in the data set.")
+  if (nrow(df_raw) < 0 || !is.data.frame(df_raw)) 
+    stop("df_raw is either empty or it's not type data.frame.")
+  
+  # if the audit directory is not passed by user, should ignore looking for audit files and it's calculation
+  if (!audit_dir_path == "") {
+    uuid <- dir(audit_dir_path)
+    if (length(uuid) > 0) {
+      # Audit Checks
+      calculate_duration <- function(audit_dir_path, uuid){
+        df <- read.csv(paste0(audit_dir_path, uuid, "/audit.csv"))
+        if (all(c("start", "end") %in% names(df))) {
+          if (nrow(df) > 0) {
+            # df <- df %>% filter(df$event != "group questions")
+            df <- df %>% filter(node != "") %>% 
+              # filter(event != "group questions") %>% 
+              mutate(
+                is_duplicate = start == lag(start),
+                time = (end - start) / 60000
+              ) %>% filter (!is_duplicate) 
+            # %>% filter(time < 10.0)
+            duration_minutes <- round(sum(df$time))
+            # duration_minutes <- round(sum(df$end - df$start)/60000, 1)
+            df_duration_calcualted <- data.frame(uuid = uuid, duration_minutes = duration_minutes)
+            return(df_duration_calcualted)  
+          }
+        }
+      }
+      
+      duration_df <- sapply(uuid, calculate_duration, audit_dir_path = audit_dir_path)
+      
+      if (all(class(duration_df) == "list")) {
+        duration_df <- do.call(rbind, duration_df)
+      } else if("matrix" %in% class(duration_df)){
+        duration_df <- t(duration_df)
+        rownames(duration_df) <- 1:nrow(duration_df)
+        duration_df <- as.data.frame(duration_df)
+        
+        duration_df$uuid <- as.character(duration_df$uuid)
+        duration_df$duration_minutes <- as.character(duration_df$duration_minutes)
+      }
+      
+    }
+  }
+  
+  #time check based on start end time
+  uuid <- df_raw[x_uuid] %>% unlist() %>% unname()
+  duration_minutes  <-  round(as.POSIXct(ymd_hms(df_raw$end)) - as.POSIXct(ymd_hms(df_raw$start))) %>% as.numeric()
+  df_no_audit_files <- data.frame(uuid, duration_minutes)
+  
+  if ("duration_df" %in% ls()) {
+    df_no_audit_files <- df_no_audit_files[!df_no_audit_files$uuid %in% duration_df$uuid, ]
+    # Join Audit checks and main data set
+    duration_df_all <- rbind(duration_df, df_no_audit_files)   
+  } else {
+    duration_df_all <- df_no_audit_files
+  }
+  
+  names(duration_df_all)[names(duration_df_all) == "uuid"] <- x_uuid
+  
+  # Merging both audit checks
+  data_duration_calculated <- df_raw %>%
+    left_join(duration_df_all) %>% 
+    dplyr:: rename(interview_duration = duration_minutes)
+  
+  return(data_duration_calculated)
+}
